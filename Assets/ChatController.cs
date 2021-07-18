2bf2627afeeaae6a186bbc102ac0fa01f7eccb1e
mchoffman <85776653+mchoffman@users.noreply.github.com>
2021-07-18 13:53:48 -0600

    Added photon plugin
    Photon plugin, plus added some networking things to chat controller

--- Assets/ChatController.cs
+++ Assets/ChatController.cs
@@ -2,35 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Chat;
+using Photon.Pun;
 
 
-public class ChatController : MonoBehaviour
+public class ChatController : MonoBehaviour, IChatClientListener
 {
     public int maxMessages = 25;
 
     public GameObject chatPanel, textObject;
     public InputField chatBox;
     public Color playerMessage, info;
-    public string username;
+    public InputField username;
+    ChatClient chatClient;
+
+    #region IChatClientListener implementation
+
+    public void DebugReturn (ExitGames.Client.Photon.DebugLevel level, string message)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnDisconnected ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnConnected ()
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnChatStateChange (ChatState state)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnGetMessages (string channelName, string[] senders, object[] messages)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnPrivateMessage (string sender, object message, string channelName)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnSubscribed (string[] channels, bool[] results)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnUnsubscribed (string[] channels)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnStatusUpdate (string user, int status, bool gotMessage, object message)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnUserSubscribed (string user, bool gotMessage)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    public void OnUserSubscribed (string user, string message)
+    {
+        throw new System.NotImplementedException ();
+    }
+    public void OnUserUnsubscribed (string user, string message)
+    {
+        throw new System.NotImplementedException ();
+    }
+
+    #endregion
+    
+    [SerializeField] string userID;
 
     [SerializeField]
     List<Message> messageList = new List<Message>();
-
+     
     // Start is called before the first frame update
     void Start()
     {
-        
+        chatClient = new ChatClient(this);
+        chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(userID));
+    }
+
+    public void Awake()
+    {
+        username.text = Login.playerName;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        chatClient.Service();
         if (chatBox.text != "")
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                SendMessageToChat(username + ": " + chatBox.text, Message.MessageType.playerMessage);
+                username.text = Login.playerName;
+                SendMessageToChat(username.text + ": " + chatBox.text, Message.MessageType.playerMessage);
                 chatBox.text = "";
             }
         }
--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -39,6 +39,8 @@ public class Login : MonoBehaviour
 
     }
 
+    
+
     // Update is called once per frame
     void Update()
     {
