26371d28aa7f00692166c08332d89312e2234b7f
MJiang18 <milesjiang18@gmail.com>
2021-07-18 19:28:12 -0400

    added comments

--- Assets/ScorecardController.cs
+++ Assets/ScorecardController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ScorecardController : MonoBehaviour
 {
     //this class will contain all of the game logic to calculate the scores based on dice values
+
+    //need to figure out how to retrieve dice values in this class
     public int scoreValue = 0;
 
     // Start is called before the first frame update
