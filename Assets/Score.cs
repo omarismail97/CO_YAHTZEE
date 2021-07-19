80eb4c5ab7ce106f2499863e0729689189a51274
MJiang18 <milesjiang18@gmail.com>
2021-07-18 23:35:53 -0400

    added score calculation logic. Ones and Twos now populate

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    //this class will contain all of the game logic to calculate the scores based on dice values
+
     public bool isSelected = false;
-    //public DiceController diceController = GameObject.Find("DiceController").GetComponent("DIceController");
-    public Die[] currentDice = new Die[5];
+
+    public static DiceController diceController;
+    public static Die[] currentDice = new Die[5];
+    private static TranscriptController transcriptController;
+    public int diceScore = 0;
 
     void Start()
     {
+        diceController = GameObject.Find("DiceController").GetComponent<DiceController>();
+        currentDice = diceController.diceObjects;
         print(gameObject.name);
     }
     public void selectScore()
@@ -19,7 +26,41 @@ public class Score : MonoBehaviour
         this.GetComponent<Text>().text = "5";
     }
 
-    public void calculateScore(){
+    public void calculateScore()
+    {
+        if (! isSelected)
+        {
+            print(gameObject.name == "Ones");
+            if (gameObject.name == "Ones")
+            {
+                diceScore = 0;
+                foreach (Die die in currentDice)
+                {
+                    if (die.dieValue == 1)
+                    {
+                        diceScore = diceScore + die.dieValue;
+                    }
+                }
+                this.GetComponent<Text>().text = diceScore.ToString();
+            }
+            if (gameObject.name == "Twos")
+            {
+                diceScore = 0;
+                foreach (Die die in currentDice)
+                {
+                    if (die.dieValue == 2)
+                    {
+                        diceScore = diceScore + die.dieValue;
+                    }
+                }
+                this.GetComponent<Text>().text = diceScore.ToString();
 
+            }
+        }
+    }
+
+    void Update()
+    {
+        calculateScore();
     }
 }
--- Assets/ScorecardController.cs
+++ Assets/ScorecardController.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class ScorecardController : MonoBehaviour
 {
-    //this class will contain all of the game logic to calculate the scores based on dice values
+    
     public int scoreValue = 0;
-    public static DiceController diceController;
-    public static Die[] currentDice = new Die[5];
     private static TranscriptController transcriptController;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        diceController = GameObject.Find("DiceController").GetComponent<DiceController>();
-        currentDice = diceController.diceObjects;
         transcriptController = GameObject.Find("TranscriptController").GetComponent<TranscriptController>();
     }
 
