361d99bbdbad986874157d4be9b86a599a7f2455
MJiang18 <milesjiang18@gmail.com>
2021-07-18 21:42:06 -0400

    changed camera size

--- Assets/ScorecardController.cs
+++ Assets/ScorecardController.cs
@@ -6,6 +6,8 @@ public class ScorecardController : MonoBehaviour
 {
     //this class will contain all of the game logic to calculate the scores based on dice values
     public int scoreValue = 0;
+    public DiceController diceController;
+
 
     // Start is called before the first frame update
     void Start()
