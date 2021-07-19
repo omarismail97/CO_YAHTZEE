94053fbf74aef75bf44ad1aef0fb32ebb0835208
MJiang18 <milesjiang18@gmail.com>
2021-07-18 21:36:05 -0400

    grab dice values

--- Assets/ScorecardController.cs
+++ Assets/ScorecardController.cs
@@ -6,10 +6,17 @@ public class ScorecardController : MonoBehaviour
 {
     //this class will contain all of the game logic to calculate the scores based on dice values
     public int scoreValue = 0;
+    public DiceController diceController;
+    public Score[] scoreList = new Score[6];
 
     // Start is called before the first frame update
     void Start()
     {
+        Die[] currentDice = new Die[5];
+        currentDice = diceController.diceObjects;
+        foreach (Die die in currentDice) {
+            print(die.dieValue);
+        }
     }
 
     // Update is called once per frame
