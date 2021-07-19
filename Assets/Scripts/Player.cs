63c01aedb93941999049127435a0d98ef8c23285
tfernan6 <87343876+tfernan6@users.noreply.github.com>
2021-07-18 22:55:04 -0400

    Add files via upload
    New file

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ namespace Yahtzee.Model
+{
+
+    public class Player : MonoBehaviour
+    {
+        private static string playerName = null;
+        private static int leaderBoardPoints = 0;
+
+        public static string GetPlayerName()
+        {
+            return playerName;
+        }
+
+        public static int GetLeaderBoardScore()
+        {
+            return leaderBoardPoints;
+        }
+
+        public static void SetPlayerName(string plName)
+        {
+            playerName = plName;
+        }
+
+
+        public static void SetLeaderBoardScore(int plPoints)
+        {
+            leaderBoardPoints = plPoints;
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
