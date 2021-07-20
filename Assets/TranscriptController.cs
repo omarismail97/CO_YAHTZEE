505a04d98cc3b752f9c5efd455079986d9b136a5
mchoffman <85776653+mchoffman@users.noreply.github.com>
2021-07-19 19:04:17 -0600

    adding color to transcript controller

--- Assets/TranscriptController.cs
+++ Assets/TranscriptController.cs
@@ -9,7 +9,7 @@ public class TranscriptController : MonoBehaviour
     public static int maxMessages = 1000000;
     public GameObject chatPanel, textObject;
     [SerializeField] List<TranscriptMessage> messageList = new List<TranscriptMessage>();
-
+    public Color login, chat, dice, scorecard, turn, player, transcript, game;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +23,11 @@ public class TranscriptController : MonoBehaviour
         ChatController chatController = GameObject.Find("ChatController").GetComponent<ChatController>();
         if (!chatController.chatBox.isFocused && Input.GetKeyDown(KeyCode.Space))
         {
-            SendMessageToTranscript("You pressed the space key!");
+            SendMessageToTranscript("You pressed the space key!", TranscriptMessage.SubsystemType.game);
         }
     }
 
-    public void SendMessageToTranscript(string text)
+    public void SendMessageToTranscript(string text, TranscriptMessage.SubsystemType subsystemType)
     {
 
         if (messageList.Count >= maxMessages) 
@@ -40,11 +40,42 @@ public class TranscriptController : MonoBehaviour
         GameObject newText = Instantiate(textObject, chatPanel.transform);
         newMessage.textObject = newText.GetComponent<Text>();
         newMessage.textObject.text = newMessage.text;
-
+        newMessage.textObject.color = TranscriptMessageTypeColor(subsystemType);
 
         messageList.Add(newMessage);
     }
 
+    Color TranscriptMessageTypeColor(TranscriptMessage.SubsystemType subsystemType)
+    {
+        Color color = login;
+
+        switch (subsystemType)
+        {
+            case TranscriptMessage.SubsystemType.chat:
+                color = chat;
+                break;
+            case TranscriptMessage.SubsystemType.dice:
+                color = dice;
+                break;
+            case TranscriptMessage.SubsystemType.scorecard:
+                color = scorecard;
+                break;    
+            case TranscriptMessage.SubsystemType.turn:
+                color = turn;
+                break;
+            case TranscriptMessage.SubsystemType.player:
+                color = player;
+                break;
+            case TranscriptMessage.SubsystemType.transcript:
+                color = transcript;
+                break;
+            case TranscriptMessage.SubsystemType.game:
+                color = game;
+                break;
+        }
+
+        return color;
+    }
 }
 
 [System.Serializable]
@@ -52,4 +83,17 @@ public class TranscriptMessage
 {
     public string text;
     public Text textObject;
+    public SubsystemType subsystemType;
+
+    public enum SubsystemType
+    {
+        login,
+        chat,
+        dice,
+        scorecard,
+        turn, 
+        player, 
+        transcript,
+        game
+    }
 }
